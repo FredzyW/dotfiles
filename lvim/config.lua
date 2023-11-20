7d33b855c178a587cf9dded79cb04c32a026a368
FredzyW <fredrik@wastring.com>
2023-11-20 09:13:07 +0100

    kitty added

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,8 +1,3 @@
--- Read the docs: https://www.lunarvim.org/docs/configuration
--- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
--- Forum: https://www.reddit.com/r/lunarvim/
--- Discord: https://discord.com/invite/Xb9B4Ny
-
 vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true, silent = true })
 lvim.keys.normal_mode["|"] = ":vsplit<CR>"
@@ -11,11 +6,46 @@ lvim.transparent_window = true
 -- vim.g.catppuccin_flavour = "latte"  -- set the flavor
 -- lvim.colorscheme = "catppuccin-latte"
 -- setup must be called before loading
+lvim.builtin.lualine.options.theme = "dracula"
 lvim.builtin.lualine.sections.lualine_b = { "lsp_progress" }
 vim.opt.wrap = true 
 vim.o.termguicolors = true
 
 
+vim.cmd[[
+  let g:jupytext_fmt = 'py'
+  let g:jupytext_style = 'hydrogen'
+
+  nmap ]x ctrih/^# %%<CR><CR>
+]]
+
+vim.keymap.set('n', '<space>r', '<cmd>MagmaEvaluateOperator<cr>')
+vim.keymap.set('n', '<space>rr', '<cmd>MagmaEvaluateLine<cr>')
+vim.keymap.set('x', '<space>r', ':<C-u>MagmaEvaluateVisual<CR>', { silent = true })
+vim.keymap.set('n', '<space>rc', '<cmd>MagmaReevaluateCell<cr>')
+vim.keymap.set('n', '<space>rf', '<cmd>IronFocus<cr>')
+vim.keymap.set('n', '<space>rh', '<cmd>IronHide<cr>')
+
+vim.cmd[[
+  function! SyncJupytext()
+    execute '!jupytext --sync %:p'
+  endfunction
+
+  command! JupytextSync call SyncJupytext()
+]]
+
+vim.cmd[[
+
+  " nnoremap <silent><expr> <LocalLeader>r  :MagmaEvaluateOperator<CR>
+  " nnoremap <silent>       <LocalLeader>rr :MagmaEvaluateLine<CR>
+  " xnoremap <silent>       <LocalLeader>r  :<C-u>MagmaEvaluateVisual<CR>
+  " nnoremap <silent>       <LocalLeader>rc :MagmaReevaluateCell<CR>
+  " nnoremap <silent>       <LocalLeader>rd :MagmaDelete<CR>
+  " nnoremap <silent>       <LocalLeader>ro :MagmaShowOutput<CR>
+
+  let g:magma_automatically_open_output = v:false
+  let g:magma_image_provider = "kitty"
+]]
 
 vim.cmd [[
   function! s:AutoCompileMarkdown()
@@ -42,6 +72,8 @@ vim.wo.number = true          -- Show the current line number
 
 lvim.plugins = {
   {"lervag/vimtex"},
-  {"rafamadriz/neon"},
-  -- {"catppuccin/nvim", name = "catppuccin", flavor = "latte"}
+  { 'dccsillag/magma-nvim', build = ':UpdateRemotePlugins' },
+  {'untitled-ai/jupyter_ascending.vim'},
+
 }
+
